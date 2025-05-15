ca795a4273d25c609def7bcb171614f75a4ccd84
JoaHuang <2123176992@qq.com>
2025-05-15 14:48:11 +0800

    fix: pricing页面添加QRCODE

--- docs/.vuepress/components/Pricing.vue
+++ docs/.vuepress/components/Pricing.vue
@@ -1,6 +1,12 @@
 <script lang="ts" setup>
-import { NButton, NCard, NGrid, NGridItem, NH1, NIcon, NSpace, NText } from "naive-ui";
+import { NButton, NCard, NGrid, NGridItem, NH1, NIcon, NSpace, NText, NModal } from "naive-ui";
 import Check from './icon/Check.vue'
+import { ref } from 'vue'
+
+const showContactModal = ref(false)
+
+const qrCode = ref('https://oss.hutool.cn/hutool/kefu/m%20p.jpg?Content-Disposition=attachment%3B%20filename%3D%22kefu%2Fm%20p.jpg%22&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=admin%2F20250515%2F%2Fs3%2Faws4_request&X-Amz-Date=20250515T064327Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&X-Amz-Signature=3d7c19f17b395a37f19b240264e81474d64f2588e820c5c98c6ce6db5467829e')
+
 let data = {
   pricing: [
     {
@@ -17,13 +23,13 @@ let data = {
       btns: [{
         text: 'Download',
         link: 'https://github.com/alist-org/alist/releases'
-      },
-      {
-        text: "Sponsor",
-        link: "/guide/sponsor.html",
-        secondary: false,
-        type: "error"
       }
+        // ,{
+        //   text: "Sponsor",
+        //   link: "/guide/sponsor.html",
+        //   secondary: false,
+        //   type: "error"
+        // }
       ]
     },
     {
@@ -43,12 +49,12 @@ let data = {
         {
           text: 'Download',
           link: 'https://github.com/alist-org/desktop-release/releases',
-        },
-        {
-          text: 'Buy',
-          link: 'https://store.nn.ci/checkout/buy/51dca247-20df-4991-8104-54ca534bcc82',
-          type: 'success'
         }
+        // ,{
+        //   text: 'Buy',
+        //   link: 'https://store.nn.ci/checkout/buy/51dca247-20df-4991-8104-54ca534bcc82',
+        //   type: 'success'
+        // }
       ]
     },
     {
@@ -88,12 +94,14 @@ if (location.pathname.startsWith("/zh/")) {
         btns: [{
           text: '下载',
           link: 'https://github.com/alist-org/alist/releases',
-        }, {
-          text: "赞助",
-          link: "/zh/guide/sponsor.html",
-          secondary: false,
-          type: "error"
-        }]
+        }
+          // , {
+          //   text: "赞助",
+          //   link: "/zh/guide/sponsor.html",
+          //   secondary: false,
+          //   type: "error"
+          // }
+        ]
       },
       {
         title: 'AList桌面版',
@@ -112,12 +120,12 @@ if (location.pathname.startsWith("/zh/")) {
           {
             text: '下载',
             link: 'https://github.com/alist-org/desktop-release/releases',
-          },
-          {
-            text: '购买',
-            link: 'https://r.nn.ci/ad',
-            type: 'success'
           }
+          // ,{
+          //   text: '购买',
+          //   link: 'https://r.nn.ci/ad',
+          //   type: 'success'
+          // }
         ]
       },
       {
@@ -132,7 +140,7 @@ if (location.pathname.startsWith("/zh/")) {
         ],
         btns: [{
           text: '联系',
-          link: 'mailto:i@nn.ci'
+          link: 'mailto:i@nn.ci',
         }]
       }
     ],
@@ -140,7 +148,11 @@ if (location.pathname.startsWith("/zh/")) {
 }
 
 function openLink(link: string) {
-  window.open(link, "_blank");
+  if (link === 'mailto:i@nn.ci') {
+    showContactModal.value = true
+  } else {
+    window.open(link, "_blank");
+  }
 }
 
 </script>
@@ -165,9 +177,30 @@ function openLink(link: string) {
       </NCard>
     </NGridItem>
   </NGrid>
+
+  <NModal v-model:show="showContactModal" preset="dialog" title="联系我们">
+    <template #default>
+      <p>请通过以下二维码联系我们：</p>
+      <div class="qr-code">
+        <img
+          :src="qrCode"
+          alt="联系我们" style="width: 300px; height: 350px;">
+      </div>
+      <!-- <p>邮箱：i@nn.ci</p> -->
+    </template>
+    <template #action>
+      <NButton @click="showContactModal = false">关闭</NButton>
+    </template>
+  </NModal>
 </template>
 
 <style scoped>
+.qr-code {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .list {
   min-height: 334px;
 }
